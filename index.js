6221dacdccb890de0dce4c4620d73f5e99dda5ce
Kathryna718 <kathrynabrown22@gmail.com>
2022-01-12 20:12:57 -0500

    Studying input and output

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function Philippine (usd){
     let total = usd *51.02 ;
     console.log(total);
-}
+
 Philippine(20.00);
+
+
+let City = "HELLO BROOKLYN"
+console.log(City);
+
+
+function FindArea(length,width,){
+    return(length*width);
+}
+ 
+ console.log(FindArea(4,2));
+
+
+ let Area = FindArea(4,2);
+ console.log(Area);
+    
